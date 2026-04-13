0cd640209e2ba561a604ba6fc48f36f018537bbf
rotkiV-V <120101460+rotkiV-V@users.noreply.github.com>
2026-04-13 10:35:11 +0200

    Add files via upload

---
+++ szallit/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace szallitas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Szallitas> szallitasok = new List<Szallitas>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            string[] adatok = File.ReadAllLines("szallit.txt");
+            for (int i = 1; i < adatok.Length; i++)
+            {
+              string[] sor = adatok[i].Split(" ");
+              szallitasok.Add(new Szallitas(Int32.Parse(sor[0]),
+                  Int32.Parse(sor[1]), Int32.Parse(sor[2]), Int32.Parse(sor[3])));
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int sors = Int32.Parse(sorszam.Text)-1;
+            feladat2.Text ="innen "+szallitasok[sors].honnan +" ide "+ szallitasok[sors].hova;
+
+
+        }
+    }
+}
---
+++ szallit/szallitas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szallitas
+{
+    internal class Szallitas
+    {
+        public int ido;
+        public int honnan;
+        public int hova;
+        public int tomeg;
+        
+        public Szallitas(int ido, int honnan, int hova, int tomeg)
+        {
+            this.ido = ido;
+            this.honnan = honnan;
+            this.hova = hova;
+            this.tomeg = tomeg;
+        }
+
+
+
+    }
+}
